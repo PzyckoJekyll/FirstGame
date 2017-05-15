15dba6b412e557744161e15cee09ecdf419ff16a
PzyckoJekyll <simoneciccarelli@MacBook-Pro-di-Simone.local>
2017-05-15 15:01:04 +0200

    enemies

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@ var cursors;
 var bullets;
 var bulletsTime=0;
 var fireButton;
-var test;
+var enemies;
 
 var GameState = {
+  
   preload: function(){
       game.load.image('stars','asset/images/background.png');
       game.load.image('spaceship','asset/images/spaceship.png');
       game.load.image('bullet','asset/images/laser.png');
+      game.load.image('enemy','asset/images/enemy.png');
       
   },
+
   create: function(){
       field = game.add.tileSprite(0,0,800,600,'stars');    
       backgroundv=2;
@@ -32,11 +35,19 @@ var GameState = {
       bullets.setAll('checkWorldBounds', true);
 
       fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+       
+      enemies = game.add.group();
+      enemies.enableBody = true;
+      enemies.physicsBodyType = Phaser.Physics.ARCADE;
 
-
+      createEnemies();
 
   },
+
   update: function(){
+
+      game.physics.arcade.overlap(bullets,enemies,collisionHandler,null,this);
+
       field.tilePosition.y +=backgroundv;
       ships.body.velocity.x =0;
       if(cursors.left.isDown){
@@ -50,8 +61,11 @@ var GameState = {
       if(fireButton.isDown){
           fireBullet();
       }
+
+      screenWrap(ships) ;
       
   } 
+
 };
 
 function fireBullet(){
@@ -64,6 +78,48 @@ function fireBullet(){
         }
 
     }
+};
+
+function createEnemies(){
+    for(var y = 0; y < 4; y++){
+         for(var x = 0; x < 10; x++){
+            var enemy = enemies.create(x*48, y*50,'enemy');
+            enemy.anchor.setTo(0.5, 0.5);
+        }
+    }
+
+    enemies.x = 100;
+    enemies.x = 50;
+
+    var tween = game.add.tween(enemies).to({x:200},2000,Phaser.Easing.Linear.None, true,0,1000,true);
+    tween.onRepeat.add(descend,this);
+};
+
+function descend(){
+    enemies.y +=10;
+}
+
+function collisionHandler(bullet,enemy){
+    bullet.kill();
+    enemy.kill();
+};
+
+function screenWrap(ships) {
+
+  if (ships.x < 0) {
+    ships.x = game.width;
+  }
+  else if (ships.x > game.width) {
+    ships.x = 0;
+  }
+
+  if (ships.y < 0) {
+    ships.y = game.height;
+  }
+  else if (ships.y > game.height) {
+    ships.y = 0;
+  }
+
 };
 
 game.state.add('GameState',GameState);
